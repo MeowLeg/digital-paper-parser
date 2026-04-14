472ecb1592100f233f690641f0cbd85201d2654d
station6945 <station6945@qq.com>
2026-04-14 08:17:59 +0800

    向量化新增新闻的title

--- resource/export_article.py
+++ resource/export_article.py
@@ -3,6 +3,7 @@
 import sqlite3
 import requests
 
+
 class ExportArticle:
     def __init__(self, db_path: str = "../daily.db", page_name: str = "舟山日报"):
         self.conn = sqlite3.connect(db_path)
@@ -30,11 +31,31 @@ class ExportArticle:
                 return False
         if article["title"].startswith("新华社"):
             return False
-        if article["author"] == "" and article["photo"] == "" and article["collaborator"] == "":
+        if (
+            article["author"] == ""
+            and article["photo"] == ""
+            and article["collaborator"] == ""
+        ):
             return False
         if article["is_xinhua"] == 1:
             return False
         return True
+
+    def __vectorize_title(self) -> None:
+        # 向量化标题
+        url = "https://develop.xinlantech.cn/zscmscore/vectorize_titles"
+        resp = requests.get(url)
+        if resp.status_code != 200:
+            print(f"向量化新增标题失败, 状态码: {resp.status_code}, 响应: {resp.text}")
+            return
+        else:
+            print(f"向量化新增标题成功, 响应: {resp.text}")
+            resp_data = resp.json()
+            if resp_data["success"]:
+                return resp_data["data"]
+            else:
+                print(f"向量化新增标题失败, 响应: {resp.text}")
+                return None
 
     def job(self):
         dump_article_ids = []
@@ -49,24 +70,27 @@ class ExportArticle:
                 pass
             if not self.__filter_article(row):
                 continue
-            print(f"POST文章: {row["title"]} ")
-            resp_data = self.__post({
-                "title": row["title"],
-                "tv_or_paper": row["site_id"],
-                "publish_year": date_eles[0],
-                "publish_month": date_eles[1],
-                "publish_day": date_eles[2],
-                "tv_url": "",
-                "page_meta_id": page_meta_id,
-                "page_name": row["page_name"],
-                "state": 1,
-                "content": row["content"],
-                "html_content": row["content"].replace("\n", "<br>"),
-                "ref_id": row["id"],
-                "duration": 0,
-                "character_count":len(row["content"]),
-                "is_collaboration": 1 if len(row["collaborator"]) > 0 else 0,
-            }, "article_from_dump")
+            print(f"POST文章: {row['title']} ")
+            resp_data = self.__post(
+                {
+                    "title": row["title"],
+                    "tv_or_paper": row["site_id"],
+                    "publish_year": date_eles[0],
+                    "publish_month": date_eles[1],
+                    "publish_day": date_eles[2],
+                    "tv_url": "",
+                    "page_meta_id": page_meta_id,
+                    "page_name": row["page_name"],
+                    "state": 1,
+                    "content": row["content"],
+                    "html_content": row["content"].replace("\n", "<br>"),
+                    "ref_id": row["id"],
+                    "duration": 0,
+                    "character_count": len(row["content"]),
+                    "is_collaboration": 1 if len(row["collaborator"]) > 0 else 0,
+                },
+                "article_from_dump",
+            )
             if resp_data is not None:
                 article_id = resp_data["data"]
                 if article_id is None or article_id <= 0:
@@ -74,38 +98,49 @@ class ExportArticle:
                 reporter_data = []
                 for reporter in row["author"].split(" "):
                     if len(reporter) > 0:
-                        reporter_data.append({
-                            "reporter_name": reporter,
-                            "reporter_category_id": 3,
-                            "score": 0
-                        })
+                        reporter_data.append(
+                            {
+                                "reporter_name": reporter,
+                                "reporter_category_id": 3,
+                                "score": 0,
+                            }
+                        )
                 for reporter in row["photo"].split(" "):
                     if len(reporter) > 0:
-                        reporter_data.append({
-                            "reporter_name": reporter,
-                            "reporter_category_id": 4,
-                            "score": 0
-                        })
+                        reporter_data.append(
+                            {
+                                "reporter_name": reporter,
+                                "reporter_category_id": 4,
+                                "score": 0,
+                            }
+                        )
                 for reporter in row["collaborator"].split(" "):
                     if len(reporter) > 0:
-                        reporter_data.append({
-                            "reporter_name": reporter,
-                            "reporter_category_id": 7,
-                            "score": 0
-                        })
+                        reporter_data.append(
+                            {
+                                "reporter_name": reporter,
+                                "reporter_category_id": 7,
+                                "score": 0,
+                            }
+                        )
                 if len(reporter_data):
-                    self.__post({
-                        "article_id": article_id,
-                        "score_basic": 0,
-                        "score_action": 0,
-                        "reporter_scores": reporter_data
-                    }, "score_for_dump")
-                dump_article_ids.append(f"{row["id"]}")
-        
+                    self.__post(
+                        {
+                            "article_id": article_id,
+                            "score_basic": 0,
+                            "score_action": 0,
+                            "reporter_scores": reporter_data,
+                        },
+                        "score_for_dump",
+                    )
+                dump_article_ids.append(f"{row['id']}")
+
         self.cursor.execute(
             f"update digital_paper set is_dump = 1 where id in ({','.join(dump_article_ids)})"
         )
         self.conn.commit()
+        # 向量化标题
+        self.__vectorize_title()
 
 
 if __name__ == "__main__":
